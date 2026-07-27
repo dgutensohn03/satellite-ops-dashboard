0354cf359db809ed9359bcf3e18be0e415de69f7
Daniel <dgutensohn03@yahoo.com>
2026-07-27 14:18:09 -0600

    Create satellite fleet configuration

---
+++ src/config/fleet.ts
@@ -0,0 +1,32 @@
+export interface FleetSatellite {
+  noradId: number;
+  name: string;
+}
+
+
+export const fleet: FleetSatellite[] = [
+  {
+    noradId: 25544,
+    name: "ISS (ZARYA)"
+  },
+  {
+    noradId: 43013,
+    name: "NOAA-20"
+  },
+  {
+    noradId: 40697,
+    name: "Sentinel-2A"
+  },
+  {
+    noradId: 39084,
+    name: "Landsat 8"
+  },
+  {
+    noradId: 27424,
+    name: "Aqua"
+  },
+  {
+    noradId: 25994,
+    name: "Terra"
+  }
+];
