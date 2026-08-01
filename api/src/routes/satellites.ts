dc4c34e60ffee185c29f2dee6f650c0f5e4ab130
Daniel <dgutensohn03@yahoo.com>
2026-07-31 20:42:23 -0600

    Fix satellite API route structure

--- api/src/routes/satellites.ts
+++ api/src/routes/satellites.ts
@@ -2,7 +2,7 @@ import { FastifyPluginAsync } from "fastify";
 import { getFleet } from "../services/fleetService.js";
 
 const routes: FastifyPluginAsync = async (app) => {
-  app.get("/fleet", async () => {
+  app.get("/satellites", async () => {
     return getFleet();
   });
 };
