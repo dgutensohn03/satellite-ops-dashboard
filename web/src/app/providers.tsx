215959ec3e30fff9aa2400d1a408dc4bcb6d3cb0
Daniel <dgutensohn03@yahoo.com>
2026-07-31 20:09:11 -0600

    Fix frontend build and restructure monorepo

--- web/src/app/providers.tsx
+++ web/src/app/providers.tsx
@@ -3,7 +3,7 @@ import {
   QueryClientProvider,
 } from "@tanstack/react-query";
 
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 
 
 const queryClient = new QueryClient();
